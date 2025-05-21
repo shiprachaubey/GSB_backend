4dc4f3566973e3c8ade5a9fab598473d7abfea0b
shiprachaubey <chaubeyshipra73@gmail.com>
2025-05-21 12:59:35 +0530

    database connected

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { connectDB } = require("./config/connectDB");
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const authRoutes = require('./routes/auth');
 
+const PORT = process.env.PORT || 9005;
+
 dotenv.config();
 const app = express();
+connectDB();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
-}).catch((err) => {
-  console.error('Database connection failed', err);
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   console.log('MongoDB connected');
+//   app.listen(process.env.PORT, () => {
+//     console.log(`Server running on port ${process.env.PORT}`);
+//   });
+// }).catch((err) => {
+//   console.error('Database connection failed', err);
+// });
+
+
+app.get("/gsb-service/v0/health", (req, res) => {
+    return res.send({ message: "gsb-service running" });
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is running on http://localhost:${process.env.PORT}`
+  );
 });
